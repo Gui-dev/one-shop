858d7fa7efd152c695500bcc4e2a71775eaca3d2
Adriano Silva <adrianon10@gmail.com>
2024-05-27 19:50:38 -0300

    feat: add sonner config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes/router'
 
@@ -7,6 +8,7 @@ export const App = () => {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | one shop" />
+      <Toaster richColors />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
