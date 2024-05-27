317c5ff3af68db50c346dbbdd4dcaa720b5ce4b9
Adriano Silva <adrianon10@gmail.com>
2024-05-27 05:09:24 -0300

    chore: add initial config

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,7 @@
+export const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="mt-5 text-3xl text-red-500">DASHBOARD</h1>
+    </div>
+  )
+}
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,7 @@
+export const SignIn = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">SignIn</h1>
+    </div>
+  )
+}
