7f9e578ef37e3665b5400bc75e779ecdc33a0280
Adriano Silva <adrianon10@gmail.com>
2024-05-28 03:29:21 -0300

    feat: add header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes/router'
 
 export const App = () => {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | one shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="oneshop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | one shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
