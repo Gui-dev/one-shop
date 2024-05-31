a13c00374b69bd7eddc6a08dc387331690a672cc
Adriano Silva <adrianon10@gmail.com>
2024-05-31 05:07:31 -0300

    feat: add react query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+
+import { queryClient } from '@/lib/react-query'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes/router'
@@ -11,7 +14,9 @@ export const App = () => {
       <ThemeProvider storageKey="oneshop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | one shop" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
