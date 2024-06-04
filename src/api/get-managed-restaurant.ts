f56da133e8c423cde92e309f4301acaa63f06f7b
Gui Silva <adrianon10@gmail.com>
2024-06-04 04:25:14 -0300

    feat: add api router get managed restaurant /managed-restaurant

---
+++ src/api/get-managed-restaurant.ts
@@ -0,0 +1,18 @@
+import { api } from '@/lib/api'
+
+interface IGetManagedRestaurantResponse {
+  id: number
+  managerId: string | null
+  name: string
+  description: string | null
+  createdAT: Date | null
+  updateAt: Date | null
+}
+
+export const getManagedRestaurant = async () => {
+  const { data } = await api.get<IGetManagedRestaurantResponse>(
+    '/managed-restaurant',
+  )
+
+  return data
+}
