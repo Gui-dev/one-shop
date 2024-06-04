49abfc60de8e46b299346de9a61c41295ab4e057
Gui Silva <adrianon10@gmail.com>
2024-06-04 04:24:39 -0300

    feat: add api router get profile /me

---
+++ src/api/get-profile.ts
@@ -0,0 +1,17 @@
+import { api } from '@/lib/api'
+
+interface IGetProfileResponse {
+  id: number
+  name: string
+  email: string
+  phone: string | null
+  role: string
+  createdAT: Date | null
+  updateAt: Date | null
+}
+
+export const getProfile = async () => {
+  const { data } = await api.get<IGetProfileResponse>('/me')
+
+  return data
+}
