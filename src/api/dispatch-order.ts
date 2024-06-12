93e27f2ab673b6e559d44c3c0876200fadcc4495
Gui Silva <adrianon10@gmail.com>
2024-06-12 04:12:20 -0300

    feat: add api route dispatch order

---
+++ src/api/dispatch-order.ts
@@ -0,0 +1,12 @@
+import { api } from '@/lib/api'
+
+export interface IDispatchOrderParams {
+  orderId: string
+}
+
+export const dispatchOrder = async ({ orderId }: IDispatchOrderParams) => {
+  await api.patch(`/orders/${orderId}`, {
+    status: 'delivering',
+  })
+  // await api.patch(`/orders/${orderId}/dispatch`)
+}
