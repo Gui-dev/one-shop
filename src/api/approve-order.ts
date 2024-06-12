0060289ad06127461637df7d9a2093d6a9c5f8fe
Gui Silva <adrianon10@gmail.com>
2024-06-12 04:11:53 -0300

    feat: add api route approve order

---
+++ src/api/approve-order.ts
@@ -0,0 +1,12 @@
+import { api } from '@/lib/api'
+
+export interface IApproveOrderParams {
+  orderId: string
+}
+
+export const approveOrder = async ({ orderId }: IApproveOrderParams) => {
+  await api.patch(`/orders/${orderId}`, {
+    status: 'processing',
+  })
+  // await api.patch(`/orders/${orderId}/approve`)
+}
