d0cbdb9dfd98da38d12daa754356023c980940f5
Gui Silva <adrianon10@gmail.com>
2024-06-12 03:21:59 -0300

    feat: add api route cancel order

---
+++ src/api/cancel-order.ts
@@ -0,0 +1,12 @@
+import { api } from '@/lib/api'
+
+export interface ICancelOrderParams {
+  orderId: string
+}
+
+export const cancelOrder = async ({ orderId }: ICancelOrderParams) => {
+  await api.patch(`/orders/${orderId}`, {
+    status: 'canceled',
+  })
+  // await api.patch(`/orders/${orderId}/cancel`)
+}
