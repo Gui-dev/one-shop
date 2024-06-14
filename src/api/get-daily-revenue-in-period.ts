a8d21c40623e1efc96ffa0469275bac5a789ad78
Gui Silva <adrianon10@gmail.com>
2024-06-14 03:51:27 -0300

    feat: add api route get daily revenue in period

---
+++ src/api/get-daily-revenue-in-period.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { api } from '@/lib/api'
+
+export interface IGetDailyRevenueInPeriod {
+  from: Date | undefined
+  to: Date | undefined
+}
+
+export type IGetDailyRevenueInPeriodResponse = {
+  date: string
+  receipt: number
+}[]
+
+export const getDailyRevenueInPeriod = async (
+  _props: IGetDailyRevenueInPeriod,
+) => {
+  const { data } = await api.get<IGetDailyRevenueInPeriodResponse>(
+    '/metrics/daily-receipt-in-period',
+  )
+
+  // const { data } = await api.get<IGetDailyRevenueInPeriodResponse>(
+  //   '/metrics/daily-receipt-in-period',
+  //   {
+  //     params: {
+  //       from
+  //       to,
+  //     },
+  //   },
+  // )
+
+  return data
+}
