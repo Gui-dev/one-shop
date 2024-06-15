612a18e51c0109a040dc4d90b7348431a1e4e30d
Gui Silva <adrianon10@gmail.com>
2024-06-15 04:41:41 -0300

    test: it should display the right text when order status is canceled

--- src/pages/app/orders/components/__tests__/order-status.spec.tsx
+++ src/pages/app/orders/components/__tests__/order-status.spec.tsx
@@ -11,4 +11,13 @@ describe('<OrderStatus />', () => {
     expect(statusText).toBeInTheDocument()
     expect(badgeElement).toHaveClass('bg-slate-400')
   })
+
+  it('should display the right text when order status is canceled', () => {
+    const wrapper = render(<OrderStatus status="canceled" />)
+
+    const statusText = wrapper.getByText('Cancelado')
+    const badgeElement = wrapper.getByTestId('badge')
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-rose-500')
+  })
 })
