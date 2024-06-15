d0e4328597a91a4085a911dd7d78b1f6778d8d3c
Gui Silva <adrianon10@gmail.com>
2024-06-15 04:40:22 -0300

    test: it should display the right text when order status is pending

--- src/pages/app/orders/components/__tests__/order-status.spec.tsx
+++ src/pages/app/orders/components/__tests__/order-status.spec.tsx
@@ -1,5 +1,14 @@
+import { render } from '@testing-library/react'
+
+import { OrderStatus } from '../order-status'
+
 describe('<OrderStatus />', () => {
-  it('1 + 1 equals 2', () => {
-    expect(1 + 1).toEqual(2)
+  it('should display the right text when order status is pending', () => {
+    const wrapper = render(<OrderStatus status="pending" />)
+
+    const statusText = wrapper.getByText('Pendente')
+    const badgeElement = wrapper.getByTestId('badge')
+    expect(statusText).toBeInTheDocument()
+    expect(badgeElement).toHaveClass('bg-slate-400')
   })
 })
