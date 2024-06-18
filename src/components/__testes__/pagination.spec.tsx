6f3eaeb041245c869c3a5a14516ffe8f13c6157b
Gui Silva <adrianon10@gmail.com>
2024-06-18 04:08:19 -0300

    test: it should be able to navigate to next page

--- src/components/__testes__/pagination.spec.tsx
+++ src/components/__testes__/pagination.spec.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Pagination } from '../pagination'
+
+const onPageChangeCallback = vi.fn()
 
 describe('<Pagination />', () => {
   it('should display the right amount of pages and results', () => {
@@ -15,5 +18,24 @@ describe('<Pagination />', () => {
 
     expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
     expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  })
+
+  it('should be able to navigate to next page', async () => {
+    const user = userEvent.setup()
+    const wrapper = render(
+      <Pagination
+        page_index={1}
+        per_page={10}
+        total_count={200}
+        onPageChange={onPageChangeCallback}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallback).toHaveBeenCalledWith(2)
   })
 })
