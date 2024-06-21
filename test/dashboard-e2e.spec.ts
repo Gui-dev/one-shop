c0f599702d495659eb6fab78a3304d99f9007c2c
Gui Silva <adrianon10@gmail.com>
2024-06-21 04:55:49 -0300

    test: add e2e test - display month orders amount metric

--- test/dashboard-e2e.spec.ts
+++ test/dashboard-e2e.spec.ts
@@ -1,8 +1,15 @@
 import { expect, test } from '@playwright/test'
 
-test('display day orders amount', async ({ page }) => {
+test('display day orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
   await expect(page.getByText('20', { exact: true })).toBeVisible()
   await expect(page.getByText('-5% em relação à ontem')).toBeVisible()
+})
+
+test('display month orders amount metric', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' })
+
+  await expect(page.getByText('200')).toBeVisible()
+  await expect(page.getByText('+7% em relação ao mês passado')).toBeVisible()
 })
