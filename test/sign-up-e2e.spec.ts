e66437d149ba10df360d6dcdc2db4cb3d79d4c50
Gui Silva <adrianon10@gmail.com>
2024-06-21 04:19:28 -0300

    test: add e2e test - sign up with error

--- test/sign-up-e2e.spec.ts
+++ test/sign-up-e2e.spec.ts
@@ -12,6 +12,20 @@ test('sign up successfully', async ({ page }) => {
 
   expect(toast).toBeVisible()
 })
+
+test('sign up with error', async ({ page }) => {
+  await page.goto('/sign-up', { waitUntil: 'networkidle' })
+  await page.getByLabel('Nome do estabelecimento').fill('Fake shop')
+  await page.getByLabel('Seu nome').fill('Fake name')
+  await page.getByLabel('E-mail').fill('fake@email.com')
+  await page.getByLabel('Seu celular').fill('11991928272')
+  await page.getByRole('button', { name: 'Finalizar cadastro' }).click()
+
+  const toast = page.getByText('Erro ao cadastrar estabelecimento')
+
+  expect(toast).toBeVisible()
+})
+
 test('navigate to login page', async ({ page }) => {
   await page.goto('/sign-up', { waitUntil: 'networkidle' })
   await page.getByRole('link', { name: 'Fazer login' }).click()
