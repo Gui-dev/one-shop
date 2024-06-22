9a3725e2acd859bbabe54b94c7678930eb4d96ca
Gui Silva <adrianon10@gmail.com>
2024-06-22 03:29:45 -0300

    test: add e2e test - list orders

---
+++ test/orders-e2e.spec.ts
@@ -0,0 +1,13 @@
+import { expect, test } from '@playwright/test'
+
+test('list orders', async ({ page }) => {
+  await page.goto('/orders', { waitUntil: 'networkidle' })
+
+  await expect(
+    page.getByRole('cell', { name: 'Customer 1', exact: true }),
+  ).toBeVisible()
+
+  await expect(
+    page.getByRole('cell', { name: 'Customer 10', exact: true }),
+  ).toBeVisible()
+})
